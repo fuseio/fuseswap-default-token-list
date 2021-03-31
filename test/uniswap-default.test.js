c8bf2e471cf1b58098e50f0c8f521bc2a004d4cf
Mulenga Bowa <mulengabowa53@gmail.com>
2021-03-31 18:32:48 +0200

    added weth

--- test/uniswap-default.test.js
+++ test/uniswap-default.test.js
@@ -1,5 +1,5 @@
 const packageJson = require('../package.json');
-const schema = require('@uniswap/token-lists/src/tokenlist.schema.json');
+const schema = require('@fuseio/token-lists/src/tokenlist.schema.json');
 const { expect } = require('chai');
 const { getAddress } = require('@ethersproject/address');
 const Ajv = require('ajv');
@@ -34,16 +34,6 @@ describe('buildList', () => {
       const key = `${token.chainId}-${token.symbol.toLowerCase()}`;
       expect(typeof map[ key ])
         .to.equal('undefined');
-      map[ key ] = true;
-    }
-  })
-
-  it('contains no duplicate names', () => {
-    const map = {};
-    for (let token of defaultTokenList.tokens) {
-      const key = `${token.chainId}-${token.name.toLowerCase()}`;
-      expect(typeof map[ key ])
-        .to.equal('undefined', `duplicate name: ${token.name}`);
       map[ key ] = true;
     }
   })
